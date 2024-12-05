62d653749d54ffc080c96e72e78bae597771436a
elohejereme14 <elohejereme14@gmail.com>
2024-12-05 20:59:40 +0800

    Save highscore

--- State_GameOver.py
+++ State_GameOver.py
@@ -12,12 +12,13 @@ import os
 class State_GameOver(BaseState):
     statename = "Game Over"
 
-    def __init__(self, sm : StateManager, rm : ResourceManager, window : pygame.Surface):
+    def __init__(self, sm: StateManager, rm: ResourceManager, window: pygame.Surface):
         super().__init__(sm, rm, window, State_GameOver.statename)
         self.backgroundColor = (100, 180, 220)
-        
-        self.levelMap = LevelMap(64) # GridSize = 64x64
-    
+        self.levelMap = LevelMap(64)  # GridSize = 64x64
+        self.high_score_file = "highscore.txt"  # Path to store the high score
+        self.high_score = 0  # Initialize high score
+
     def __drawMap(self):
         Tiles = LevelMap.Tiles
         dimension = self.levelMap.mapDim
@@ -30,16 +31,14 @@ class State_GameOver(BaseState):
                     self.AddDrawSprite(Tiles[value], position)
 
     def __handleKeyInput(self):
-        # Trigger once
-        # Toggle Debug
         for env in self.eventlist:
-            if env.type == pygame.KEYDOWN:            
+            if env.type == pygame.KEYDOWN:
                 if env.key == K_RETURN:
                     self.sm.ChangeState("Main Menu")
                     self.rm.GetAudioClip("Selecting").Play()
 
     def __drawUIs(self):
-        if self.sm.variables["Lives"] == -1: # Died
+        if self.sm.variables["Lives"] == -1:  # Died
             self.AddDrawUIFont("You Died!", Vector2(32, 64), MYCOLOR.RED, 72)
             self.AddDrawUIFont(f'Levels Completed : {self.sm.variables["CurrentLevel"] - 1} / {self.sm.variables["NumOfLevels"]}', Vector2(32, 150), MYCOLOR.WHITE, 30)
         else:
@@ -48,24 +47,40 @@ class State_GameOver(BaseState):
 
         self.AddDrawUIFont(f'Total Score Gained : {self.sm.variables["totscore"]}', Vector2(32, 180), MYCOLOR.WHITE, 30)
         self.AddDrawUIFont(f'Time Elapsed : {int(self.sm.variables["TimeTaken"] / 60)}m {int(self.sm.variables["TimeTaken"] % 60)}s', Vector2(32, 210), MYCOLOR.WHITE, 30)
+        self.AddDrawUIFont(f'Highest Score : {self.high_score}', Vector2(32, 250), MYCOLOR.WHITE, 30)
 
-        self.AddDrawUIFont("[Enter] Return to main menu", Vector2(32, 250), MYCOLOR.RED, 25)
-            
+        self.AddDrawUIFont("[Enter] Return to main menu", Vector2(32, 290), MYCOLOR.RED, 25)
 
     def Load(self):
         super().Load()
         self.levelMap.LoadMap(os.path.join("Assets", "Level", 'GameOverScreen.dat'))
         self.rm.GetAudioClip("MainMenuBGM").source.play(loops=-1)
-    
+
+        # Load the high score from the file
+        if os.path.exists(self.high_score_file):
+            with open(self.high_score_file, "r") as file:
+                try:
+                    self.high_score = int(file.read().strip())
+                except ValueError:
+                    self.high_score = 0
+        else:
+            self.high_score = 0
+
+        # Update high score if necessary
+        current_score = self.sm.variables["totscore"]
+        if current_score > self.high_score:
+            self.high_score = current_score
+            with open(self.high_score_file, "w") as file:
+                file.write(str(self.high_score))
+
     def Unload(self):
         super().Unload()
         self.rm.GetAudioClip("MainMenuBGM").source.stop()
-    
+
     def Update(self, dt):
         self.__handleKeyInput()
-        # Draw screen
         self.__drawMap()
         self.__drawUIs()
-
         super().Update(dt)
         super().Draw()
+
